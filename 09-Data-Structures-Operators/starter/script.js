e125c8eb871d92cdb41786f368fbf5591840d3e2
Corrigan Newsome <corrigannewsome@gmail.com>
2022-02-03 16:33:39 -0700

    using set, has, and delete method of maps

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,27 +1,41 @@
 "use strict";
 
 // Data needed for a later exercise
-const flights =
-  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+// const flights =
+//   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
 // Data needed for first part of the section
+const weekdays = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
 const restaurant = {
   name: "Classico Italiano",
   location: "Via Angelo Tavanti 23, Firenze, Italy",
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
+  order(starterIndex, mainIndex) {
+    return this.starterMenu[starterIndex], this.mainMenu[mainIndex];
+  },
+  orderDelievered({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delievered to ${address} at ${time}`
+    );
+  },
 
   openingHours: {
-    thu: {
+    [weekdays[3]]: {
       open: 12,
       close: 22,
     },
-    fri: {
+    [weekdays[4]]: {
       open: 11,
       close: 23,
     },
-    sat: {
+    [weekdays[5]]: {
       open: 0, // Open 24 hours
       close: 24,
     },
@@ -131,3 +145,23 @@ GOOD LUCK 😀
 // //7.
 // team1 < team2 && console.log("Team 1 is more likely to win");
 // team2 > team1 && console.log("Team 2 is more likely to win");
+
+//LEARNING MAPS
+const rest = new Map();
+rest.set("name", "Classico Italiano");
+rest.set(1, "Firenze, Italy");
+rest.set(2, "Lisbon, Portugal");
+
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23);
+
+rest.get("name");
+
+console.log(rest.get("name"));
+
+console.log(rest.has("categories"));
+
+rest.delete(2);
+console.log(rest);
