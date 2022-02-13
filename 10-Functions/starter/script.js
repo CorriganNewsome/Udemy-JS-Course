db28c26c56baad35fc051e89a956ffb69c362f2c
Corrigan Newsome <corrigannewsome@gmail.com>
2022-02-12 18:57:07 -0600

    default params in ES6

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -2,9 +2,11 @@
 
 const bookings = [];
 
-const createBooking = (flightNum, numPassengers, price) => {
-  numPassengers = numPassengers || 1;
-
+//ES6
+//Write equals and then the default paramater you want.
+const createBooking = (flightNum, numPassengers = 1, price = 199) => {
+  //ES5
+  // numPassengers = numPassengers || 1;
   const booking = {
     flightNum,
     numPassengers,
