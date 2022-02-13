8eb870810d958f937a7e8d1da05d8bf625272d73
Corrigan Newsome <corrigannewsome@gmail.com>
2022-02-12 18:49:33 -0600

    ES5 way of making default parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,17 @@
-'use strict';
+"use strict";
+
+const bookings = [];
+
+const createBooking = (flightNum, numPassengers, price) => {
+  numPassengers = numPassengers || 1;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("LH123");
