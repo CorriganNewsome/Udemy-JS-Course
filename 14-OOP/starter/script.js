726a0f3c2756139e213952b4b643b70c9b9aee03
Corrigan Newsome <corrigannewsome@gmail.com>
2022-02-20 11:09:08 -0600

    constructor function practice

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,16 @@
-'use strict';
+"use strict";
+
+//constructor function
+const Person = function (firstName, birthyear) {
+  this.firstName = firstName;
+  this.birthyear = birthyear;
+};
+
+const jonas = new Person("Jonas", 1991);
+console.log(jonas);
+
+//BTS of the 'new' keyword
+// 1. New object is created
+// 2. Function is called, this = ().
+// 3. Object is linked to a prototype
+// 4. Function automatically retuns a object with the passed data.
