b5fdc34aff6d5bfee2ffe274ffed02469566d6dc
Corrigan Newsome <corrigannewsome@gmail.com>
2022-02-20 19:28:30 -0600

    commented some code

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -2,15 +2,37 @@
 
 //constructor function
 const Person = function (firstName, birthyear) {
+  //instance properties
   this.firstName = firstName;
   this.birthyear = birthyear;
+
+  //Method
+
+  /*
+  Never use a method inside of a constructor function, because then everytime you run this funciton you will
+  also be bringing along this method which you may not necessairly need. Use protoypes instead for that 
+  functionality. 
+  
+  this.calcAge = function () {
+     console.log(2037 - this.birthyear);
+   };
+  */
 };
 
 const jonas = new Person("Jonas", 1991);
-console.log(jonas);
 
 //BTS of the 'new' keyword
 // 1. New object is created
 // 2. Function is called, this = ().
 // 3. Object is linked to a prototype
 // 4. Function automatically retuns a object with the passed data.
+
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("Jack", 1975);
+
+const jay = "Jay";
+
+console.log(jonas instanceof Person);
+console.log(jay instanceof Person);
+
+//Prototypes and Prototypal Inheritance
