38a3922eae8eb90e735cfd4108ccccce1ad6d127
Corrigan Newsome <corrigannewsome@gmail.com>
2022-10-10 20:56:53 -0500

    messing with functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,29 @@
-'use strict';
+"use strict";
+
+const flight = "234";
+const jonas = {
+  name: "Jonas Schmedtmann",
+  passport: 2489345094835,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Mr." + passenger.name;
+
+  if (passenger.passport === 2489345094835) {
+    alert("Checked In");
+  } else {
+    alert("Wrong Passport");
+  }
+};
+
+//checkIn(flight, jonas);
+//console.log(flight);
+//console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
